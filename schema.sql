888bc8572e1ce9c7a22042c61895d2cac6d7835d
Nikolas Mentakis <nikolasmentakis@its-MacBook-Pro.local>
2020-04-01 13:02:54 -0500

    Add package.json for backend

--- schema.sql
+++ schema.sql
@@ -2,6 +2,29 @@ DROP DATABASE IF EXISTS fullstackdrill;
 
 CREATE DATABASE fullstackdrill;
 
+USE fullstackdrill;
 
-CREATE TABLE IF NOT EXISTS employees (id integer AUTO_INCREMENT UNIQUE PRIMARY KEY, first_name varchar(255),last_name varchar(255),city varchar(255));
+CREATE TABLE IF NOT EXISTS employees (
+  id integer AUTO_INCREMENT UNIQUE PRIMARY KEY,
+  first_name varchar(255),
+  last_name varchar(255),
+  city varchar(255)
+);
 
+ 
+
+INSERT INTO employees (first_name, last_name, city) VALUES ("Suman", "Rush", "Menlo Park");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Marco", "Cooke", "Valdez");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Anabelle", "Howell", "Vallejo");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Alistair", "Grey", "Estes Park");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Bayley", "Brennan","Fredericksburg");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Shakira", "Harrington", "Grand Island");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Jayson", "Schaefer", "Reno");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Shaurya", "Pugh", "Lafayette");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Mikolaj", "Christian", "Glendive");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Lily", "Taylor", "Lubbock");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Henley", "Almond", "Lake Placid");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Pharrell", "Cohen", "Woodbridge");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Amina", "Andrade", "Lock Haven");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Alan", "Davey", "Orange");
+INSERT INTO employees (first_name, last_name, city) VALUES ("Jaeden", "West", "Hays");
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 3000;
-const queries = require('queries');
+const path = require("path");
+const PORT = 3001;
+const queries = require("./queries");
 
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-app.get('/employees', (req, res)=>{
-    // run your query here
-    res.send('hello from the server!')
-})
+app.get("/employees", (req, res) => {
+  // run your query here
+  res.send("hello from the server!");
+});
 
-
-app.listen(PORT, ()=>{
-    console.log(`server is running and listening on port ${PORT}`); 
-})
+app.listen(PORT, () => {
+  console.log(`server is running and listening on port ${PORT}`);
+});
